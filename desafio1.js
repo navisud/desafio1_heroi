6061a9f87528c6876d5faf3e23301d3562f8fcd2
abraham <abrahamluccas13@gmail.com>
2024-06-21 19:30:31 -0300

    first commit

---
+++ desafio1.js
@@ -0,0 +1,27 @@
+let nome = "Adalto";
+let exp = 9050;
+
+if (exp < 1000) {
+    console.log("O herói de nome "+ nome +" está no nível: Ferro")
+}
+if (exp >= 1001 && exp <= 2000) {
+    console.log("O herói de nome "+ nome +" está no nível: Bronze")
+}
+if (exp >= 2001 && exp <= 5000) {
+    console.log("O herói de nome "+ nome +" está no nível: Prata")
+}
+if (exp >= 6001 && exp <= 7000) {
+    console.log("O herói de nome "+ nome +" está no nível: Ouro")
+}
+if (exp >= 7001 && exp <= 8000) {
+    console.log("O herói de nome "+ nome +" está no nível: Platina")
+}
+if (exp >= 8001 && exp <= 9000) {
+    console.log("O herói de nome "+ nome +" está no nível: Ascendente")
+}
+if (exp >= 9001 && exp <= 10000) {
+    console.log("O herói de nome "+ nome +" está no nível: Imortal")
+}
+if (exp >= 10001) {
+    console.log("O herói de nome "+ nome +" está no nível: Radiante")
+}
